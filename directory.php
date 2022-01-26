1245ad26e501e10daf361c95173c8c4d05226e86
devonbarks <63268642+devonbarks@users.noreply.github.com>
2022-01-25 23:02:32 -0500

    added now playing feature

--- directory.php
+++ directory.php
@@ -3,12 +3,30 @@
     <title>LastFM</title>
     <script src='index.php'></script>
     <body>
+    <?php
+        require 'lastfm.php';
+        $lastfm = new LastFM("092d316884d8385f35ad8b84f5f42ef8");
+        $tracks = $lastfm->getRecentTracks('devonbarks');
+    ?>
+
     <h1>Devon's LastFM Statistics!</h1>
         <ul>
             <li><a href="/recent.php">Recently Played</li>
-            <li><a href="/topTracks.php">Top played Tracks</li>
+            <li><a href="/topTracks.php">Top played Tracks</a></li>
             <li><a href="/collage.php">Top played Albums</li>
-            <li><a href="/friends.php">friends</li>
+            <li><a href="/friends.php">Friends</a></li>
         </ul>
+        <h3>Now playing:</h3>
+        <?php $i = 0;
+        foreach($tracks->track as $k=>$v): ?>
+                <?php if (empty($v->date)) { ?>
+                <span class="name"><?php echo $v->name; ?></span>
+                <span>   by    </span>
+                <span class="artist"><?php echo $v->artist; ?></span>
+                <?php } ?>
+        <?php 
+        $i++;
+        if($i == 1) break;
+        endforeach; ?>
     </body>
 </html>
