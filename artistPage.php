2afe1daac8d3ae98c8f478e9b59ffa321ab1b4bb
devonbarks <63268642+devonbarks@users.noreply.github.com>
2022-04-05 13:53:10 -0400

    fixed spacing

--- artistPage.php
+++ artistPage.php
@@ -18,28 +18,26 @@
         <title><?= $artist ?></title>
     </head>
     <body style="background-color: #f8e3d8">
-    <div class="container">
-        <div class="container-column left">
-            <?php if ($artist === null): ?>
-                <h1>artist not found.</h1>
-            <?php else: ?>
-            <h1 style="padding-right: 0%;"><?= $artist ?></h1>
-            <?php endif; ?>
-        
-        </div>
-        <div class="container-column right">
-        <?php foreach($artistInfo->artist as $k=>$v): ?>
+        <div class="container">
+            <div class="container-column left">
+                <?php if ($artist === null): ?>
+                    <h1>artist not found.</h1>
+                <?php else: ?>
+                <h1 style="padding-right: 0%;"><?= $artist ?></h1>
+                <?php endif; ?>
+            </div>
+            <div class="container-column right">
+                <?php foreach($artistInfo->artist as $k=>$v): ?>
                     <img src="<?php echo $v->image[3]; ?>" alt="unable to load" style="bottom:720px; border: 5px solid #000; float:right;">
-            <?php endforeach; ?>
-            <br>
-        <?php foreach($artistInfo->artist as $k=>$v):
-            foreach($v->bio as $f=>$g): 
-                $string = substr($g->summary, 0, strpos($g->summary, "<a"))?>
-                <p><?= $string; ?></p>
-        <?php endforeach;
-            endforeach; ?>
+                <?php endforeach; ?>
+                <br>
+                <?php foreach($artistInfo->artist as $k=>$v):
+                   foreach($v->bio as $f=>$g): 
+                    $string = substr($g->summary, 0, strpos($g->summary, "<a"))?>
+                    <p><?= $string; ?></p>
+                <?php endforeach;
+                endforeach; ?>
+            </div>
         </div>
-    </div>
-        
     </body>
 </html>
--- songPage.php
+++ songPage.php
@@ -10,38 +10,37 @@
 
 <!-- Loads a different song based on what was clicked from the index page -->
 
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <link rel="stylesheet" href='style.css'>
+        <link rel="stylesheet" href='stylesheets/artist.css'>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><?= $track ?> | <?= $artist ?></title>
     </head>
     <body style="background-color: #f8e3d8">
-    <section>
-        <?php if ($artist === null || $track === null): ?>
-                <h3>song not found.</h3>
-        <?php else: ?>
-            <h3><?= $artist ?></h3>
-            <h1><?= $track ?></h1>
-        <?php endif; ?>
-        
-    </section>
-    <aside style="float: right;">
-        <?php foreach($trackInfo->track as $k=>$v): 
-                foreach($v->album as $f=>$g):?>
-                    <img src="<?php echo $g->image[3]; ?>" alt="unable to load" style="right:50px; bottom:720px; border: 5px solid #000; float:right;">
-            <?php endforeach; 
-        endforeach;?>
-        <?php foreach($trackInfo->track as $k=>$v):
-            foreach($v->wiki as $f=>$g): 
-                $string = substr($g->summary, 0, strpos($g->summary, "<a"))?>
-                <p style="float: right;"><?= $string; ?></p>
-        <?php endforeach;
-            endforeach; ?>
-    </aside>
-        
+        <div class="container">
+            <div class="container-column left">
+                <?php if ($artist === null || $track === null): ?>
+                    <h3>song not found.</h3>
+                <?php else: ?>
+                    <h3><?= $artist ?></h3>
+                    <h1><?= $track ?></h1>
+                <?php endif; ?>
+            </div>
+            <div class="container-column right">
+                <?php foreach($trackInfo->track as $k=>$v): 
+                    foreach($v->album as $f=>$g):?>
+                        <img src="<?php echo $g->image[3]; ?>" alt="unable to load" style="right:50px; bottom:720px; border: 5px solid #000; float:right;">
+                <?php endforeach; 
+                endforeach;
+                foreach($trackInfo->track as $k=>$v):
+                  foreach($v->wiki as $f=>$g): 
+                    $string = substr($g->summary, 0, strpos($g->summary, "<a"))?>
+                    <p style="float: right;"><?= $string; ?></p>
+                    <?php endforeach;
+                endforeach; ?>
+            </div>
+        </div>
     </body>
 </html>
