2ca834973913a40e1fa0db468623e0d6e225a6d4
devonbarks <63268642+devonbarks@users.noreply.github.com>
2022-04-05 13:46:25 -0400

    fixed containers

--- artistPage.php
+++ artistPage.php
@@ -13,30 +13,33 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <link rel="stylesheet" href='style.css'>
+        <link rel="stylesheet" href='stylesheets/artist.css'>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><?= $artist ?></title>
     </head>
     <body style="background-color: #f8e3d8">
-    <section>
-        <?php if ($artist === null): ?>
-                <h3>artist not found.</h3>
-        <?php else: ?>
-            <h3><?= $artist ?></h3>
-        <?php endif; ?>
+    <div class="container">
+        <div class="container-column left">
+            <?php if ($artist === null): ?>
+                <h1>artist not found.</h1>
+            <?php else: ?>
+            <h1 style="padding-right: 0%;"><?= $artist ?></h1>
+            <?php endif; ?>
         
-    </section>
-    <aside style="float: right;">
+        </div>
+        <div class="container-column right">
         <?php foreach($artistInfo->artist as $k=>$v): ?>
-                    <img src="<?php echo $v->image[3]; ?>" alt="unable to load" style="right:50px; bottom:720px; border: 5px solid #000; float:right;">
+                    <img src="<?php echo $v->image[3]; ?>" alt="unable to load" style="bottom:720px; border: 5px solid #000; float:right;">
             <?php endforeach; ?>
+            <br>
         <?php foreach($artistInfo->artist as $k=>$v):
             foreach($v->bio as $f=>$g): 
                 $string = substr($g->summary, 0, strpos($g->summary, "<a"))?>
-                <p style="float: right;"><?= $string; ?></p>
+                <p><?= $string; ?></p>
         <?php endforeach;
             endforeach; ?>
-    </aside>
+        </div>
+    </div>
         
     </body>
 </html>
